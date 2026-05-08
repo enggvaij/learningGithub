783f77e116d9642c1cd40d9ac3bf48fdc8a2b7bb
enggvaij <vaijayanti.shingade@gmail.com>
2026-05-08 12:27:12 +0100

    merge 3

--- gittest.py
+++ gittest.py
@@ -5,13 +5,12 @@ if (hungry =="yes"):
     print("Eat burger")
     print("Eat fries")
 else:
-<<<<<<< HEAD
     thirsty = input("Are you thirsty?")
     if thirsty=="yes":
         print("Drink water!")
         print("Drink soda as well!")
     
     
-=======
+
     print("Do your Homework.")
->>>>>>> 86a79d17cd31b9ac159d2aac5ae738ab30c31aa8
+
