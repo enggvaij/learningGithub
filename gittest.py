01c96e78ad1a12841bb0841a165437151d772894
enggvaij <vaijayanti.shingade@gmail.com>
2026-05-08 11:52:40 +0100

    fires-pizza-burger-samosa

--- gittest.py
+++ gittest.py
@@ -1,5 +1,8 @@
 hungry = input("Are you Hungery?")
 if (hungry =="yes"):
     print("Eat Samosa")
+    print("Eat pizza")
+    print("Eat burger")
+    print("Eat fries")
 else:
     print("Do your Homework.")
